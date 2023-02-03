64035f77145d0e611d76c2dfe677f81674c044a0
Sergio-Gual <sergiogual.istf@gmail.com>
2023-02-02 23:16:13 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Clasificar from './components/Clasificar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app container">
+      <div className="jumbotron">
+        <h1 className="lead text-center">Sistema Web para la optimización de la poscosecha </h1>
+         <Clasificar /> 
+      </div>
     </div>
   );
 }
---
+++ src/components/Clasificar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Clasificar() {
+    return (
+      <div class="d-grid gap-2">
+        <button type="button" class="btn btn-outline-info">
+          Clasificar
+        </button>
+      </div> 
+    );
+  }
